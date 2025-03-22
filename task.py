299df7d715d9cb91577014872163f071ee1f76a4
Glynn St. Juste <gstjuste95@gmail.com>
2025-03-22 19:37:22 -0400

    Add files via upload

---
+++ task.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+# Finds what the tip is, and updates the bill
+bill += tip/100.0*bill
+
+# final line
+bill_with_tip = round(bill/people,2)
+print(f"Each person should pay: ${bill_with_tip}")
