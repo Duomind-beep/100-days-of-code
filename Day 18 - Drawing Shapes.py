6ad5b4d71af84737a5d8b9007330d014514cf7c3
Glynn St. Juste <gstjuste95@gmail.com>
2025-04-02 22:11:34 -0400

    Update and rename Drawing Shapes.py to Day 18 - Drawing Shapes.py

--- Day 18 - Drawing Shapes.py
+++ Day 18 - Drawing Shapes.py
@@ -5,11 +5,7 @@ timmy = Turtle()
 timmy.shape("classic")
 timmy.color("teal")
 
-
-
-
 color_bank = ["royal blue", "pale violet red", "sea green", "light sky blue", "dark orange"]
-
 
 num_sides = 2
 for _ in range(10):
@@ -19,9 +15,5 @@ for _ in range(10):
         timmy.right(360 / num_sides)
         timmy.color(choice(color_bank))
 
-
-
-
 screen = Screen()
-# screen.screensize(5000,5000)
 screen.exitonclick()
