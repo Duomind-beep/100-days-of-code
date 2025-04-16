0052cdcdb60af68aa47aad9875a8d11df9ae5d8f
Glynn St. Juste <gstjuste95@gmail.com>
2025-04-16 16:16:48 -0400

    Update snake.py
    Snake resets after a game over

--- Day 21 - Snake Game/snake.py
+++ Day 21 - Snake Game/snake.py
@@ -24,7 +24,15 @@ class Snake:
         new_segment.color('orange')
         new_segment.goto(position)
         self.segments.append(new_segment)
-
+        
+    # resets snake as a game over
+      def game_over(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+    # the extension mechanic for the snake
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
