883f54962a1d6666d3562edf3c9bc006ca782e71
Glynn St. Juste <gstjuste95@gmail.com>
2025-04-24 17:05:00 -0400

    Add files via upload

--- Day 26 - NATO Alphabet Project/main.py
+++ Day 26 - NATO Alphabet Project/main.py
@@ -2,6 +2,15 @@ import pandas
 
 nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter:row.code for (index,row) in nato_data.iterrows()}
-chosen_word = input("Enter a word: ").upper()
-phonetic_code = [nato_dict[letter] for letter in chosen_word]
-print(phonetic_code)
+
+def nato_coder():
+    chosen_word = input("Enter a word: ").upper()
+
+    try:
+        phonetic_code = [nato_dict[letter] for letter in chosen_word]
+    except KeyError:
+        nato_coder()
+    else:
+        print(phonetic_code)
+
+nato_coder()
